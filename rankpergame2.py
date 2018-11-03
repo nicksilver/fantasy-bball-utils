f750ee0d67a963680414202be0db623a349db997
nick <nick.silverman11@gmail.com>
2018-11-03 06:57:41 -0600

    Finished creating stats table without user data

--- rankpergame2.py
+++ rankpergame2.py
@@ -13,6 +13,7 @@ Be prepared to login to espn every time you run this script
 
 # dependencies
 import pandas as pd
+import numpy as np
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
@@ -27,9 +28,8 @@ html = driver.page_source
 soup = BeautifulSoup(html, "lxml")
 
 #### These next few lines write the html to a file, which can be helpful
-# import sys
-# sys.stdout = open('div2.txt', 'w')
-# soup
+# with open('html_sample.txt', 'w') as file:
+#     file.write(str(soup))
 
 # scrape team names
 teams = soup.find_all('span', class_ = "teamName truncate")
@@ -39,52 +39,60 @@ for team in teams:
     team_names.append(name)
 
 team_names = team_names[0:10]
+my_name = soup.find_all('span', class_= "Nav__Text clr-gray-04 n8 pl3 truncate")
+my_name_clean = str(my_name).split(">")[1].split("<")[0]
 
 stats = soup.find_all('div', class_='jsx-2810852873 table--cell stat-value tar')
 stats_clean = [str(i).split(">")[1].split("<")[0] for i in stats]
+gp = stats_clean[-9:]
+stat_names = ["FG%", "FT%", "3PM", "REB", "AST", "STL", "BLK", "PTS"]
+ind = team_names
+ind.remove(my_name_clean)
+df_stats = pd.DataFrame(np.asarray(stats_clean[:-9]).reshape([9, 8]), 
+    columns=stat_names, index=ind)
+df_stats['GP'] = gp
 
-
-
+#TODO Find my_team's stats
 
 
 ###### Old code
-""" def get_stats(stat_name):
-    """function to scrape stats"""
-    stats = soup.find_all('td', class_=stat_name)
-    lis = []
-    for stat in stats:
-        n = float(stat.find(text=True))
-        lis.append(n)
-    return lis
+# def get_stats(stat_name):
+#     """function to scrape stats"""
+#     stats = soup.find_all('td', class_=stat_name)
+#     lis = []
+#     for stat in stats:
+#         n = float(stat.find(text=True))
+#         lis.append(n)
+#     return lis
 
-# scrape stats
-fg = get_stats('precise sortableStat19')  # field goal percentages
-ft = get_stats('precise sortableStat20')  # free throw percentages
-tpm = get_stats('precise sortableStat17')  # three pointers made
-reb = get_stats('precise sortableStat6')  # rebounds
-ast = get_stats('precise sortableStat3')  # assists
-stl = get_stats('precise sortableStat2')  # steals
-blk = get_stats('precise sortableStat1')  # blocks
-pts = get_stats('precise sortableStat0')  # points
-gp = get_stats('sortableGP')  # games played
-moves = get_stats('sortableMoves')  # number of moves
+# # scrape stats
+# fg = get_stats('precise sortableStat19')  # field goal percentages
+# ft = get_stats('precise sortableStat20')  # free throw percentages
+# tpm = get_stats('precise sortableStat17')  # three pointers made
+# reb = get_stats('precise sortableStat6')  # rebounds
+# ast = get_stats('precise sortableStat3')  # assists
+# stl = get_stats('precise sortableStat2')  # steals
+# blk = get_stats('precise sortableStat1')  # blocks
+# pts = get_stats('precise sortableStat0')  # points
+# gp = get_stats('sortableGP')  # games played
+# moves = get_stats('sortableMoves')  # number of moves
 
-# create dataframe
-dic = {'FG': fg, 'FT': ft, '3PM': tpm, 'REB': reb, 'AST': ast, 'STL': stl,
-       'BLK': blk, 'PTS': pts, 'GP': gp, 'MOVES': moves}
-df = pd.DataFrame(dic, index=team_names)
+# # create dataframe
+# dic = {'FG': fg, 'FT': ft, '3PM': tpm, 'REB': reb, 'AST': ast, 'STL': stl,
+#        'BLK': blk, 'PTS': pts, 'GP': gp, 'MOVES': moves}
+# df = pd.DataFrame(dic, index=team_names)
 
-# calculate stats per games played
-df_new = df.div(df.GP, axis='index')
-df_new.FG = df.FG
-df_new.FT = df.FT
-df_new.GP = df.GP
-df_new.MOVES = df.MOVES
+# # calculate stats per games played
+# df_new = df.div(df.GP, axis='index')
+# df_new.FG = df.FG
+# df_new.FT = df.FT
+# df_new.GP = df.GP
+# df_new.MOVES = df.MOVES
 
-# rescore and rank
-df_ranks = df_new.rank(axis=0, method='average')
-del df_ranks['MOVES']
-del df_ranks['GP']
-df_ranks['TOTAL'] = df_ranks.sum(axis=1)
+# # rescore and rank
+# df_ranks = df_new.rank(axis=0, method='average')
+# del df_ranks['MOVES']
+# del df_ranks['GP']
+# df_ranks['TOTAL'] = df_ranks.sum(axis=1)
 
-print df_ranks.sort_values('TOTAL', ascending=False) """
+# print df_ranks.sort_values('TOTAL', ascending=False)

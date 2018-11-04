c3bd0459f1bcb019a3ca04e52d4aff20c1a8cf45
nick <nick.silverman11@gmail.com>
2018-11-04 10:50:23 -0700

    deleted unused libraries

--- rankpergame2.py
+++ rankpergame2.py
@@ -19,11 +19,8 @@ import requests
 from bs4 import BeautifulSoup
 import time
 from selenium import webdriver
-from selenium.common.exceptions import TimeoutException
-from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.ui import WebDriverWait # available since 2.4.0
 from selenium.webdriver.support import expected_conditions as EC # available since 2.26.0
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
 # Open website, automatically login, and scrape html
